41222a43c4d631540a2d33a7870cfd521c054fc1
tarik sghiouri idrissi <tarik.sghiouriidrissi@usmba.ac.ma>
2023-04-14 22:36:39 +0000

    test

--- Kmeans/kmeans.py
+++ Kmeans/kmeans.py
@@ -1,6 +1,81 @@
 from math import sqrt
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from numpy.random import uniform
+from sklearn.datasets import make_blobs
+import seaborn as sns
+import random
 
 def euclidean_distance(x, y):
     return sqrt(sum((px - py)**2  for px, py in zip(x,y)))
-    
-print(euclidean_distance((1,9),(2,5)))
+
+def most_commom(list):
+    return max(set(list),key=list.count)
+
+# print(euclidean_distance((1,9),(2,5)))
+class KMeans:
+    def __init__(self, n_clusters=8, max_iter=100):
+        self.n_clusters = n_clusters
+        self.max_iter = max_iter
+    def fit(self, X_train):
+        for _ in range(self.n_clusters-1):
+            # Calculate distances from points to the centroids
+            dists = np.sum([euclidean_distance(centroid, X_train) for centroid in self.centroids], axis=0)
+            # Normalize the distances
+            dists /= np.sum(dists)
+            # Choose remaining points based on their distances
+            new_centroid_idx = np.random.choice(range(len(X_train)), size=1, p=dists)[0]  # Indexed @ zero to get val, not array of val
+            self.centroids += [X_train[new_centroid_idx]]
+        iteration = 0
+        while np.not_equal(self.centroids, prev_centroids).any() and iteration < self.max_iter:
+            # Sort each datapoint, assigning to nearest centroid
+            sorted_points = [[] for _ in range(self.n_clusters)]
+            for x in X_train:
+                dists = euclidean_distance(x, self.centroids)
+                centroid_idx = np.argmin(dists)
+                sorted_points[centroid_idx].append(x)
+
+            # Push current centroids to previous, reassign centroids as mean of the points belonging to them
+            prev_centroids = self.centroids
+            self.centroids = [np.mean(cluster, axis=0) for cluster in sorted_points]
+            for i, centroid in enumerate(self.centroids):
+                if np.isnan(centroid).any():  # Catch any np.nans, resulting from a centroid having no points
+                    self.centroids[i] = prev_centroids[i]
+            iteration += 1
+    def evaluate(self, X):
+        centroids = []
+        centroid_idxs = []
+        for x in X:
+            dists = euclidean_distance(x, self.centroids)
+            centroid_idx = np.argmin(dists)
+            centroids.append(self.centroids[centroid_idx])
+            centroid_idxs.append(centroid_idx)
+
+        return centroids, centroid_idxs
+
+# Create a dataset of 2D distributions
+centers = 5
+X_train, true_labels = make_blobs(n_samples=100, centers=centers, random_state=42)
+X_train = StandardScaler().fit_transform(X_train)
+# Fit centroids to dataset
+kmeans = KMeans(n_clusters=centers)
+kmeans.fit(X_train)
+
+# View results
+class_centers, classification = kmeans.evaluate(X_train)
+sns.scatterplot(x=[X[0] for X in X_train],
+                y=[X[1] for X in X_train],
+                hue=true_labels,
+                style=classification,
+                palette="deep",
+                legend=None
+                )
+plt.plot([x for x, _ in kmeans.centroids],
+         [y for _, y in kmeans.centroids],
+         '+',
+         markersize=10,
+         )
+plt.title("k-means")
+plt.show()
+
